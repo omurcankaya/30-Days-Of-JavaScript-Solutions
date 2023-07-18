a7f327bd03283425549411d60ab7769d5a53a2bf
Ömürcan Kaya <omurcankaya@protonmail.com>
2023-07-18 21:44:51 +0300

    Exercise 3 Solved and Added!

--- Day-9/day9.js
+++ Day-9/day9.js
@@ -143,34 +143,27 @@ console.log(categorizeCountries(countries, patterns));
 
 // 2.4
 function startWithCountry(countries) {
-    const letterAndCount = []
+    const letterAndCount = [{
+        letter: 'A',
+        count: 0
+    }];
     countries.forEach(country => {
         const firstLetter = country.slice(0, 1);
-        if (letterAndCount.length === 0) {
+        if (letterAndCount.some(element => element.letter === firstLetter)) {
+            letterAndCount.forEach(element => {
+                if (element.letter === firstLetter) {
+                    element.count += 1;
+                }
+            })
+        } else {
             const newLetter = {
                 letter: firstLetter,
                 count: 1,
-                country: country
             };
             letterAndCount.push(newLetter);
-        } else {
-            if (letterAndCount.some(element => element.letter === firstLetter)) {
-                letterAndCount.forEach(element => {
-                    if (element.letter === firstLetter) {
-                        element.count += 1;
-                        element.country = `${element.country}, ${country}`;
-                    }
-                })
-            } else {
-                const newLetter = {
-                    letter: firstLetter,
-                    count: 1,
-                    country: country
-                };
-                letterAndCount.push(newLetter);
-            }
         }
-    });
+    }
+    );
     return letterAndCount;
 }
 
@@ -199,3 +192,156 @@ initialLetters.forEach(element => {
 console.log(initialLetters[maxIndex]);
 
 // Exercises 3
+
+// 3.1
+
+// Name sort 
+console.log(countries_data.sort());
+// Capital Sort
+const sorted = countries_data.filter(element => typeof element['capital'] === 'string').sort((a, b) => a['capital'].localeCompare(b['capital']));
+console.log(sorted.concat(countries_data.filter((element) => typeof element['capital'] !== 'string')));
+// Population Sort
+console.log(countries_data.sort((a, b) => a['population'] - b['population']));
+
+// 3.2
+const languagesListAndCount = [{
+    language: 'English',
+    count: 0
+}];
+const mostSpokenLanguages = (countries, length) => {
+    countries.filter((country) => typeof country.languages === 'object').forEach((country) => {
+        country.languages.forEach((language) => {
+            if (languagesListAndCount.some((languageAndCount) => languageAndCount.language === language)) {
+                languagesListAndCount[languagesListAndCount.findIndex((element) => language === element.language)].count += 1;
+            } else {
+                const newLanguage = {
+                    language: language,
+                    count: 1
+                };
+                languagesListAndCount.push(newLanguage);
+
+            }
+        })
+    })
+    languagesListAndCount.sort((a, b) => b.count - a.count);
+    return languagesListAndCount.filter(element => languagesListAndCount.indexOf(element) < length);
+}
+
+console.log(mostSpokenLanguages(countries_data, 5));
+
+// 3.3
+
+function mostPopulationCountry(countries, length) {
+    const sorted = countries.sort((a, b) => b.population - a.population).map((item) => {
+
+        return { country: item.name, population: item.population };
+    });
+    return sorted.filter(item => sorted.indexOf(item) < length);
+}
+console.log(mostPopulationCountry(countries_data, 5));
+
+// 3.4
+
+const statistics = {
+    ages: [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26],
+    count: () => {
+        return statistics.ages.length;
+    },
+    sum: () => {
+        return statistics.ages.reduce((acc, curr) => acc + curr);
+    },
+    min: () => {
+        const sorted = statistics.ages.sort((a, b) => a - b);
+        return sorted[0]
+    },
+    max: () => {
+        const sorted = statistics.ages.sort((a, b) => b - a);
+        return sorted[0]
+    },
+    range: () => {
+        return (statistics.max() - statistics.min());
+    },
+    median: () => {
+        const sorted = statistics.ages.sort((a, b) => a - b);
+        if (statistics.count % 2 === 0) {
+            return (sorted[sorted.length / 2] + sorted[sorted.length / 2 + 1]) / 2;
+        } else {
+            return sorted[Math.floor(sorted.length / 2)];
+        }
+    },
+    mean: () => {
+        return statistics.sum() / statistics.count();
+    },
+    mode: () => {
+        const modObject = [{
+            age: statistics.ages[0],
+            count: 0
+        }];
+        statistics.ages.forEach((age) => {
+            if (modObject.some((modItem) => modItem.age === age)) {
+                modObject[modObject.findIndex((modItem) => modItem.age === age)].count += 1;
+            } else {
+                const newModItem = {
+                    age: age,
+                    count: 1
+                };
+                modObject.push(newModItem);
+            }
+        });
+
+
+        const sorted = modObject.sort((a, b) => b.count - a.count);
+        return sorted[0];
+    },
+    var: () => {
+        let total = 0;
+        statistics.ages.forEach(age => {
+            total += (statistics.mean() - age) ** 2
+        })
+
+        total = total / (statistics.count());
+        return total;
+    },
+    std: () => {
+        return (statistics.var() ** (1 / 2));
+    },
+    freqDist: () => {
+        const modObject = [{
+            age: statistics.ages[0],
+            count: 0
+        }];
+        statistics.ages.forEach((age) => {
+            if (modObject.some((modItem) => modItem.age === age)) {
+                modObject[modObject.findIndex((modItem) => modItem.age === age)].count += 1;
+            } else {
+                const newModItem = {
+                    age: age,
+                    count: 1
+                };
+                modObject.push(newModItem);
+            }
+        });
+        const arrFreqDist=[];
+        modObject.forEach((modItem) => {
+            console.log(modItem);
+            const newFreq = {
+                percent: 100*modItem.count / statistics.count(),
+                age: modItem.age
+            };
+            arrFreqDist.push(newFreq);
+        });
+        return arrFreqDist.sort((a,b)=>b.percent-a.percent);
+    }
+};
+
+console.log('Count:', statistics.count());
+console.log('Sum: ', statistics.sum());
+console.log('Min: ', statistics.min());
+console.log('Max: ', statistics.max());
+console.log('Range: ', statistics.range());
+console.log('Mean: ', statistics.mean());
+console.log('Median: ', statistics.median());
+console.log('Mode: ', statistics.mode());
+console.log('Variance: ', statistics.var());
+console.log('Standard Deviation: ', statistics.std());
+console.log('Frequency Distribution: ', statistics.freqDist());
