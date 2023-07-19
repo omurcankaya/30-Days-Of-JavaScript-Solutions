4c12361610154ad047d0006e22788e44f7948a64
Ömürcan Kaya <omurcankaya@protonmail.com>
2023-07-19 10:54:06 +0300

    Day 10 and Excercise 1 Solutions Added!

---
+++ Day-10/day10.js
@@ -0,0 +1,35 @@
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+const countries = ['Finland', 'Sweden', 'Norway']
+
+// Exercise 1
+
+// 1.1
+const emptySet = new Set();
+
+// 1.2
+const zeroToTenSet = new Set();
+for(let i =0;i<=10;i++){
+    zeroToTenSet.add(i);
+}
+console.log(zeroToTenSet);
+
+// 1.3
+zeroToTenSet.delete(10);
+console.log(zeroToTenSet);
+
+// 1.4
+zeroToTenSet.clear();
+console.log(zeroToTenSet);
+
+// 1.5
+const array = ['Slm','mrb','nbr','iyi','sen']
+const stringSet = new Set(array);
+console.log(stringSet);
+
+// 1.6
+const countriesAndCount = new Map();
+countries.forEach((element) => countriesAndCount.set(element,element.length));
+console.log(countriesAndCount);
+
+// Exercise 2
