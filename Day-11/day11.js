b63a8e9570c994422b5a326312b28e1297109043
Ömürcan Kaya <omurcankaya@protonmail.com>
2023-07-21 13:59:29 +0300

    countries_data added and solved Exercises 3

--- Day-11/day11.js
+++ Day-11/day11.js
@@ -80,3 +80,64 @@ for ({ name, skills } of users) {
         console.log(name, skills);
     }
 }
+
+Exercises 3
+
+3.1
+for ({ name, capital, population, languages } of countries_data) {
+    console.log(name, capital, population, languages);
+}
+
+// 3.2
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
+const [firstName, skills, [, , jsScore, reactScore]] = student;
+console.log(firstName, skills, jsScore, reactScore);
+
+// 3.3
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+const convertArrayToObject = (studentArray) => {
+    const result = []
+    for ([firstName2, skills2, scores2] of studentArray) {
+        const newStudent = {
+            name: firstName2,
+            skills: skills2,
+            scores: scores2
+        }
+        result.push(newStudent);
+    }
+    return result;
+};
+
+console.log(convertArrayToObject(students));
+
+// 3.4
+const student2 = {
+    name: 'David',
+    age: 25,
+    skills: {
+        frontEnd: [
+            { skill: 'HTML', level: 10 },
+            { skill: 'CSS', level: 8 },
+            { skill: 'JS', level: 8 },
+            { skill: 'React', level: 9 }
+        ],
+        backEnd: [
+            { skill: 'Node', level: 7 },
+            { skill: 'GraphQL', level: 8 },
+        ],
+        dataBase: [
+            { skill: 'MongoDB', level: 7.5 },
+        ],
+        dataScience: ['Python', 'R', 'D3.js']
+    }
+};
+
+const newStudent = {...student2};
+newStudent.skills.frontEnd.push({ skill: 'Bootstrap', level: 8 });
+newStudent.skills.backEnd.push({ skill: 'Express', level: 9 });
+newStudent.skills.dataBase.push({ skill: 'SQL', level: 8 });
+newStudent.skills.dataScience.push('SQL');
+console.log(newStudent);
