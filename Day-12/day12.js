ee3217221984cd5348c1981febbdfc2592d770a9
Ömürcan Kaya <omurcankaya@protonmail.com>
2023-07-22 15:45:33 +0300

    Day 12, Exercise 3 Solved

--- Day-12/day12.js
+++ Day-12/day12.js
@@ -42,3 +42,16 @@ console.log(tenMostFrequentWords(paragraph));
 console.log(tenMostFrequentWords(paragraph,3));
 
 // Exercise 3
+
+// 3.1
+const cleanText = (paragraph) =>{
+    return paragraph.replace(/[^a-zA-Z ]/g,'')
+};
+sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+console.log(cleanText(sentence));
+
+// 3.2
+function cleanAndMostFrequent (paragraph){
+    return tenMostFrequentWords(cleanText(paragraph), 3);
+}
+console.log(cleanAndMostFrequent(sentence));
