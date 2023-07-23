db34715faeeb8121560629485a86183a01abdb97
Ömürcan Kaya <omurcankaya@protonmail.com>
2023-07-23 10:42:31 +0300

    Day 13 Exercise 1 Added and README.md Updated

---
+++ Day-13/day13.js
@@ -0,0 +1,47 @@
+// Exercise 1
+
+// 1.1
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+]
+console.table(countries);
+
+// 1.2
+const countries_data = [
+    {
+        "name": "Afghanistan",
+        "capital": "Kabul",
+        "languages": [
+            "Pashto",
+            "Uzbek",
+            "Turkmen"
+        ],
+        "population": 40218234,
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
+        "region": "Asia",
+        "area": 652230
+    },
+    {
+        "name": "Åland Islands",
+        "capital": "Mariehamn",
+        "languages": [
+            "Swedish"
+        ],
+        "population": 28875,
+        "flag": "https://flagcdn.com/ax.svg",
+        "region": "Europe",
+        "area": 1580
+    }
+];
+console.table(countries_data);
+
+// 1.3
+console.group('Errors');
+console.error('Error 1');
+console.error('Error 2');
+console.error('Error 3');
+console.groupEnd();
+
+// Exercise 2
--- README.md
+++ README.md
@@ -16,4 +16,5 @@ You can search like that in files. **X.Y**\
 [Day 9](https://github.com/omurcankaya/30Days-Of-JavaScript-Solutions/blob/main/Day-09/day9.js)\
 [Day 10](https://github.com/omurcankaya/30Days-Of-JavaScript-Solutions/blob/main/Day-10/day10.js)\
 [Day 11](https://github.com/omurcankaya/30Days-Of-JavaScript-Solutions/blob/main/Day-11/day11.js)\
-[Day 12](https://github.com/omurcankaya/30Days-Of-JavaScript-Solutions/blob/main/Day-12/day12.js)
+[Day 12](https://github.com/omurcankaya/30Days-Of-JavaScript-Solutions/blob/main/Day-12/day12.js)\
+[Day 13](https://github.com/omurcankaya/30Days-Of-JavaScript-Solutions/blob/main/Day-13/day13.js)
